8149a140dce5c30d860b18961f5fe6e864b75d21
RobinCZW <596954885@qq.com>
2016-01-28 20:03:01 +0800

    服务页面换gridview

--- app/src/main/java/com/example/robin/papers/demo/activity/FragmentService2.java
+++ app/src/main/java/com/example/robin/papers/demo/activity/FragmentService2.java
@@ -14,7 +14,10 @@ import com.example.robin.papers.demo.util.MyGridView;
 public class FragmentService2 extends Fragment{
 
     private MyGridView gridview_services,gridView_links;
-    public String[] img_text_service = { "单期绩点", "大物实验", "打印上门", "快递代领", "器材正装", "表白神器",
+
+
+    //福大服务的图标和名字
+    public String[] img_text_service = { "单期绩点", "抢大物实验", "打印上门", "快递代领", "器材正装", "表白神器",
             "福大纪念品", "服务入驻" ,"投诉反馈"};
     public int[] imgs_service = { R.drawable.zcpm,
             R.drawable.qsy,
@@ -26,7 +29,9 @@ public class FragmentService2 extends Fragment{
             R.drawable.jyhhz,
             R.drawable.jyhhz };
 
-    public String[] img_text_links = { "Papers主页","新庭芳苑", "福大易班", "教务处", "人才联合网"," "," "," "," "," "," "," "," "," "
+
+    //友情链接的图标和名字
+    public String[] img_text_links = { "Papers主页","新庭芳苑", "福大易班", "教务处", "大物实验","学工系统","人才联合网","申请友链"," "
             };
     public int[] imgs_links = { R.drawable.zcpm,
             R.drawable.xtfy,
@@ -36,20 +41,16 @@ public class FragmentService2 extends Fragment{
             R.drawable.zhaopin,
             R.drawable.zhaopin,
             R.drawable.zhaopin,
-            R.drawable.zhaopin,
-            R.drawable.zhaopin,
-            R.drawable.zhaopin,
-            R.drawable.zhaopin,
-            R.drawable.zhaopin,
-            R.drawable.fxq};
+            R.drawable.zhaopin
+            };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_fragment_service2,container,false);
 
-        gridView_links = (MyGridView) view.findViewById(R.id.gridviewLinks);
         gridview_services =(MyGridView) view.findViewById(R.id.gridviewServices);
+        gridView_links = (MyGridView) view.findViewById(R.id.gridviewLinks);
 
         gridview_services.setAdapter(new MyGridAdapter(getActivity(), img_text_service, imgs_service));
         gridView_links.setAdapter(new MyGridAdapter(getActivity(), img_text_links, imgs_links));

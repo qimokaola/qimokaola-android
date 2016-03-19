44271934d0084413ed99c4b5833b1759319a4dcc
RobinCZW <596954885@qq.com>
2016-03-19 15:18:21 +0800

    集成友盟统计, 推送集成ing

--- app/src/main/java/com/example/robin/papers/demo/activity/AdsActivity.java
+++ app/src/main/java/com/example/robin/papers/demo/activity/AdsActivity.java
@@ -2,51 +2,26 @@ package com.example.robin.papers.demo.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.robin.papers.R;
 import com.umeng.analytics.MobclickAgent;
-import com.umeng.update.UmengUpdateAgent;
 
-
-public class SplashActivity extends Activity {
-
-    private LinearLayout splash_layout;
-    private boolean loginState;
+public class AdsActivity extends Activity {
+    private RelativeLayout splash_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_splash);
-
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//
-//
-//        new Handler().postDelayed(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-//                SplashActivity.this.startActivity(mainIntent);
-//                SplashActivity.this.finish();
-//            }
-//
-//        }, 3000);
-
-
-
-
-
-
+        setContentView(R.layout.activity_ads);
         //找到splash整个页面的linearlayout
-        splash_layout = (LinearLayout) findViewById(R.id.splash_layout);
+        splash_layout = (RelativeLayout) findViewById(R.id.ads_activity);
 
         //渐进的效果 两秒钟
         AlphaAnimation aa = new AlphaAnimation(0.0f, 1.0f);
@@ -55,13 +30,7 @@ public class SplashActivity extends Activity {
         // 完成窗体的全屏显示 // 取消掉状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        //获取登录状态
-        SharedPreferences preferences = getSharedPreferences("loginState",MODE_PRIVATE);
-        loginState = preferences.getBoolean("state",false);
-
-
-        //用线程让splash页面停留2秒钟再进入AdsActivity
+        //用线程让splash页面停留2秒钟再进入RegistActivity
         new Handler().postDelayed(new Runnable() {
 
             @Override
@@ -75,14 +44,13 @@ public class SplashActivity extends Activity {
 //                }
 //                intent.setClass(SplashActivity.this,clazz);
 //                startActivity(intent);
-                startActivity(new Intent(SplashActivity.this,AdsActivity.class));
-                SplashActivity.this.finish();
+                startActivity(new Intent(AdsActivity.this, MainActivity.class));
+                AdsActivity.this.finish();
             }
 
-        }, 1000);
-
-
+        }, 3000);
     }
+
 
     public void onResume() {
         super.onResume();

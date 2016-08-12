bcab27ebb0d6d8510f8d2acde7ddc02c971df4d4
RobinCZW <596954885@qq.com>
2016-08-12 17:45:28 +0800

    UI微调

--- app/src/main/java/com/example/robin/papers/activity/SplashActivity.java
+++ app/src/main/java/com/example/robin/papers/activity/SplashActivity.java
@@ -55,7 +55,7 @@ public class SplashActivity extends Activity {
                                 @Override
                                 public void run() {
                                     try {
-                                        Thread.sleep(1000);
+                                        Thread.sleep(3000);
                                     } catch (InterruptedException e) {
                                         e.printStackTrace();
                                     }

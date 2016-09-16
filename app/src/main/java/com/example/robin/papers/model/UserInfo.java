c2510a9a32588a673cd7699a981a4ec107e2915b
imspace <imspace@vip.qq.com>
2016-09-16 21:19:29 +0800

    添加自动setter，getter插件，lambda语法糖

---
+++ app/src/main/java/com/example/robin/papers/model/UserInfo.java
@@ -0,0 +1,15 @@
+package com.example.robin.papers.model;
+
+import lombok.Data;
+
+/**
+ * Created by space on 16/9/16.
+ * 存放用户信息
+ */
+public @Data
+class UserInfo {
+    private String nickname;
+    UserInfo() {
+
+    }
+}
